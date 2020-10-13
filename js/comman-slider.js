cd6927d1b9654ad096b64c40e86f07d55097c6d5
Mayank-MP05 <mayank5pande@gmail.com>
2020-10-13 12:56:33 +0530

    Slider on rooftop page

---
+++ js/comman-slider.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".comman-slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+        fade: true,
+        cssEase: "linear",
+      },
+    ],
+  });
+});
